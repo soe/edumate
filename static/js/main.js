97958a2a03b207a71d7ca0d51b0dde1ad0d40777
Soe <soe@soe.im>
2012-03-25 04:49:08 +0200

    did some pages :)

--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
   $('a[rel=tooltip]').tooltip();
   $('#credits').popover({'placement': 'top', });
   
+  
+  $('#target_pls').on('change', function(){
+    $('#targeted').slideToggle();
+  });
+    
 });
